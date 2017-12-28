e738b4df0cddc4f74594090d59e9ba0bcb8ce2dc
Lucie Hammond <lucie.hammond@student.ecp.fr>
2017-12-28 19:33:56 +0100

    create slides for help section and design the introduction slide

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-help',
-  templateUrl: 'help.html',
+	selector: 'page-help',
+	templateUrl: 'help.html',
 })
 export class HelpPage {
+	@ViewChild(Slides) slides: Slides;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HelpPage');
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad HelpPage');
+	}
+
+	nextSlide() {
+		this.slides.slideNext();
+	}
 
 }
