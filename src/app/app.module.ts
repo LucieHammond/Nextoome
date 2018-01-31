9e2292633d556c1ff2670b4fcbd20474621d1043
Lucie Hammond <lucie.hammond@student.ecp.fr>
2018-01-31 10:57:07 +0100

    add customer-service and contact pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,76 +1,84 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { HttpClientModule } from '@angular/common/http';
-import { Camera } from '@ionic-native/camera';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {HttpClientModule} from '@angular/common/http';
+import {Camera} from '@ionic-native/camera';
+import {InAppBrowser} from '@ionic-native/in-app-browser';
+import {EmailComposer} from '@ionic-native/email-composer';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { FlashSalePage } from '../pages/flash-sale/flash-sale';
-import { UserProfilePage } from '../pages/user-profile/user-profile';
-import { WishlistPage } from '../pages/wishlist/wishlist';
-import { BasketPage } from '../pages/basket/basket';
-import { OrdersPage } from '../pages/orders/orders';
-import { NextoosPage } from '../pages/nextoos/nextoos';
-import { CustomerServicePage } from '../pages/customer-service/customer-service';
-import { HelpPage } from '../pages/help/help';
-import { ParamsPage } from '../pages/params/params';
-import { ProductPage } from '../pages/product/product';
-import { PicturePage } from '../pages/picture/picture';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {FlashSalePage} from '../pages/flash-sale/flash-sale';
+import {UserProfilePage} from '../pages/user-profile/user-profile';
+import {WishlistPage} from '../pages/wishlist/wishlist';
+import {BasketPage} from '../pages/basket/basket';
+import {OrdersPage} from '../pages/orders/orders';
+import {NextoosPage} from '../pages/nextoos/nextoos';
+import {CustomerServicePage} from '../pages/customer-service/customer-service';
+import {HelpPage} from '../pages/help/help';
+import {ParamsPage} from '../pages/params/params';
+import {ProductPage} from '../pages/product/product';
+import {PicturePage} from '../pages/picture/picture';
+import {ContactPage} from '../pages/contact/contact'
 
-import { ApiConnectorService } from '../services/api-connector';
-import { SessionInfos } from '../services/session-infos';
-import { SharedBasket } from '../services/shared-basket';
+import {ApiConnectorService} from '../services/api-connector';
+import {SessionInfos} from '../services/session-infos';
+import {SharedBasket} from '../services/shared-basket';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    FlashSalePage,
-    UserProfilePage,
-    WishlistPage,
-    BasketPage,
-    OrdersPage,
-    NextoosPage,
-    HelpPage,
-    CustomerServicePage,
-    ParamsPage,
-    ProductPage,
-	PicturePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-    HttpClientModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    FlashSalePage,
-    UserProfilePage,
-    WishlistPage,
-    BasketPage,
-    OrdersPage,
-    NextoosPage,
-    HelpPage,
-    CustomerServicePage,
-    ParamsPage,
-    ProductPage,
-	PicturePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-	Camera,
-    ApiConnectorService,
-    SharedBasket,
-	SessionInfos
-  ]
+	declarations: [
+		MyApp,
+		HomePage,
+		FlashSalePage,
+		UserProfilePage,
+		WishlistPage,
+		BasketPage,
+		OrdersPage,
+		NextoosPage,
+		HelpPage,
+		CustomerServicePage,
+		ParamsPage,
+		ProductPage,
+		PicturePage,
+		ContactPage
+	],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(MyApp),
+		HttpClientModule
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		HomePage,
+		FlashSalePage,
+		UserProfilePage,
+		WishlistPage,
+		BasketPage,
+		OrdersPage,
+		NextoosPage,
+		HelpPage,
+		CustomerServicePage,
+		ParamsPage,
+		ProductPage,
+		PicturePage,
+		ContactPage
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{provide: ErrorHandler, useClass: IonicErrorHandler},
+		Camera,
+		ApiConnectorService,
+		SharedBasket,
+		SessionInfos,
+		InAppBrowser,
+	  	EmailComposer
+	]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/customer-service/customer-service.ts
+++ src/pages/customer-service/customer-service.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {InAppBrowser} from '@ionic-native/in-app-browser';
+import {ContactPage} from '../contact/contact'
+
 
 @IonicPage()
 @Component({
-  selector: 'page-customer-service',
-  templateUrl: 'customer-service.html',
+	selector: 'page-customer-service',
+	templateUrl: 'customer-service.html',
 })
 export class CustomerServicePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams, private iab: InAppBrowser) {
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CustomerServicePage');
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad CustomerServicePage');
+	}
 
+	openWithBrowser(url: string) {
+		this.iab.create(url, '_system', 'location=yes');
+	}
+
+	displayForm(){
+		this.navCtrl.push(ContactPage);
+	}
 }
